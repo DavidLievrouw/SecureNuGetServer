0ce16a21889319d79e05ae9e0243507b4cf5087d
David Lievrouw <dlievrouw2@csc.com>
2018-01-16 14:23:41 +0100

    Add BasicAuthentication code, with configurable credentials in web.config.

---
+++ src/SecureNuGetServer/BasicAuthentication/Configuration/ExcludeElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Configuration;
+
+namespace SecureNuGetServer.BasicAuthentication.Configuration
+{
+  public class ExcludeElement : ConfigurationElement
+  {
+    private const string UrlAttribute = "url";
+    private const string VerbAttribute = "verb";
+
+    /// <summary>
+    ///   Gets or sets the url to exclude.
+    /// </summary>
+    /// <value>
+    ///   The url.
+    /// </value>
+    [ConfigurationProperty(UrlAttribute, IsRequired = false, IsKey = false)]
+    public string Url
+    {
+      get => Convert.ToString(this[UrlAttribute]);
+      set => this[UrlAttribute] = value;
+    }
+
+    /// <summary>
+    ///   Gets or sets the verb to exclude.
+    /// </summary>
+    /// <value>
+    ///   The verb.
+    /// </value>
+    [ConfigurationProperty(VerbAttribute, IsRequired = false, IsKey = false)]
+    public string Verb
+    {
+      get => Convert.ToString(this[VerbAttribute]);
+      set => this[VerbAttribute] = value;
+    }
+
+    public override string ToString()
+    {
+      return string.Concat(Url, '_', Verb);
+    }
+  }
+}
