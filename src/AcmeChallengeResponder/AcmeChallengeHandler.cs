753a0f2ca64202072ec6e9bf80005a0516c6d226
David Lievrouw <dlievrouw2@csc.com>
2018-02-15 08:57:04 +0100

    Add AcmeChallengeResponder solution, that responds when LetsEncrypt validates a certificate request

---
+++ src/AcmeChallengeResponder/AcmeChallengeHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+
+namespace AcmeChallengeResponder
+{
+  public class AcmeChallengeHandler : IHttpHandler
+  {
+    public void ProcessRequest(HttpContext context)
+    {
+      context.Response.StatusCode = 200;
+      context.Response.ContentType = "text/plain";
+      context.Response.Write("acme-challenge-response " + Guid.NewGuid());
+    }
+
+    public bool IsReusable => true;
+  }
+}
---
+++ src/AcmeChallengeResponder/default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="AcmeChallengeResponder.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Acme challenge responder</title>
+</head>
+<body>
+  <div>
+    <% Response.Write(Guid.NewGuid().ToString()); %>
+  </div>
+</body>
+</html>
